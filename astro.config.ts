13233e6b9e5bf8084cf8bbd35ef4d61520572ccf
Tom Arend <tom@Toms-MacBook-Air.local>
2025-08-11 14:35:38 +0200

    Configure Astro site and add Pages deploy workflow

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: 'https://tom-arend.github.io', 
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
